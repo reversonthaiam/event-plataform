1977d89530370fdb06a73ae969ee17f9a79feaac
Reverson Thaiam <reverson.santos@frotabank.com.br>
2022-06-30 09:10:55 -0300

    project finished

---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+// import { Container } from './styles';
+
+import Logo from '../assets/Logo'
+
+const Header = () => {
+  return (
+    <header className="w-full py-5 flex items-center justify-center bg-gray-700 border-b border-gray-600">
+      <Logo />
+      
+    </header>
+  )
+}
+
+export default Header
